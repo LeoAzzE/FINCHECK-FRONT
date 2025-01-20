57007e0d32e63db4d0b4a26114140c7520e8c571
Borges dev <leosull50@hotmail.com>
2025-01-20 16:33:30 -0300

    iniciando o front

---
+++ frontend/src/Router/index.tsx
@@ -0,0 +1,25 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { AuthGuard } from "./AuthGuard";
+import { Register } from "../view/pages/Register";
+import { Login } from "../view/pages/Login";
+import { Dashboard } from "../view/pages/Dashboard";
+import { AuthLayout } from "../view/layouts/AuthLayout";
+
+export function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AuthGuard isPrivate={false} />}>
+                    <Route element={<AuthLayout />}>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </Route>
+                </Route>
+
+                <Route element={<AuthGuard isPrivate={false} />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
