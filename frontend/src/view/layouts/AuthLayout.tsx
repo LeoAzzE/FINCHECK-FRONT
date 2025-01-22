ba958df3fb26eeab900f18be9a2c24899ab02213
Borges dev <leosull50@hotmail.com>
2025-01-21 21:05:08 -0300

    tela default do to login

--- frontend/src/view/layouts/AuthLayout.tsx
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -1,11 +1,31 @@
 import { Outlet } from "react-router-dom";
+import illustration from "../../assets/illustration.png";
+import { Logo } from "../components/Logo";
 
 export function AuthLayout() {
     return (
-        <div>
-            <h1>AuthLAYOUT</h1>
+        <div className="flex w-full h-full">
+            <div className=" w-full lg:w-1/2 h-full flex items-center justify-center flex-col">
+                <Logo className="h-6 text-gray-500" />
 
-            <Outlet />
+                <div className="w-full px-8 max-w-[504px]">
+                    <Outlet />
+                </div>
+            </div>
+            <div className=" w-1/2 h-full justify-center items-center p-8 relative hidden lg:flex">
+                <img
+                    src={illustration}
+                    className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]"
+                />
+
+                <div className="max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-[32px]">
+                    <Logo className="text-teal-900 h-8" />
+                    <p className="text-gray-700 font-medium text-xl mt-6">
+                        Gerencie suas finanças pessoais de uma forma simples com
+                        o fincheck, e o melhor, totalmente de graça!
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
