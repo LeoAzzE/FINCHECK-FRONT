910a2bd2aa749154622f2e9603ceec29d037823c
Borges dev <leosull50@hotmail.com>
2025-01-25 15:46:42 -0300

    feat: add react hook form and zod in login page

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
-import { Input } from "../../components/input";
+import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+import { useLoginController } from "./useLoginController";
 
 export function Login() {
+    const { handleSubmit, register, errors } = useLoginController();
     return (
         <>
             <header className="flex flex-col items-center gap-4 text-center">
@@ -21,13 +23,22 @@ export function Login() {
                     </Link>
                 </p>
             </header>
-            <form className="mt-[60px] flex flex-col gap-4">
-                <Input name="email" type="email" placeholder="E-mail"></Input>
+            <form
+                onSubmit={handleSubmit}
+                className="mt-[60px] flex flex-col gap-4"
+            >
                 <Input
-                    name="password"
+                    {...register("email")}
+                    type="email"
+                    placeholder="E-mail"
+                ></Input>
+                {errors.email && <span>{errors.email.message}</span>}
+                <Input
+                    {...register("password")}
                     type="password"
                     placeholder="Senha"
                 ></Input>
+                {errors.password && <span>{errors.password.message}</span>}
                 <Button type="submit" className="mt-2">
                     Entrar
                 </Button>
