d9f434fd8a8ed391f7f1c69d16e027ba7de9ca61
Borges dev <leosull50@hotmail.com>
2025-02-15 15:47:09 -0300

    feat: all layouts done

--- frontend/src/view/pages/Dashboard/components/DashBoardContext/index.tsx
+++ frontend/src/view/pages/Dashboard/components/DashBoardContext/index.tsx
@@ -3,9 +3,13 @@ import { createContext, useCallback, useState } from "react";
 interface DashBoardContextValue {
     areValuesVisible: boolean;
     isNewAccountModalOpen: boolean;
+    isNewTransactionModalOpen: boolean;
+    newTransactionType: "INCOME" | "EXPENSE" | null;
     toggleValuesVisibility(): void;
     closeNewAccountModal(): void;
     openNewAccountModal(): void;
+    closeNewTransactionModal(): void;
+    openNewTransactionModal(type: "INCOME" | "EXPENSE"): void;
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -13,7 +17,12 @@ export const DashboardContext = createContext({} as DashBoardContextValue);
 
 export function DashBoardProvider({ children }: { children: React.ReactNode }) {
     const [areValuesVisible, setAreValuesVisible] = useState(true);
-    const [isNewAccountModalOpen, setIsNewAccountModalOpen] = useState(true);
+    const [isNewAccountModalOpen, setIsNewAccountModalOpen] = useState(false);
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+        useState(false);
+    const [newTransactionType, setNewTransactionType] = useState<
+        "INCOME" | "EXPENSE" | null
+    >(null);
 
     const toggleValuesVisibility = useCallback(() => {
         setAreValuesVisible((prevState) => !prevState);
@@ -27,6 +36,19 @@ export function DashBoardProvider({ children }: { children: React.ReactNode }) {
         setIsNewAccountModalOpen(false);
     }, []);
 
+    const openNewTransactionModal = useCallback(
+        (type: "INCOME" | "EXPENSE") => {
+            setNewTransactionType(type);
+            setIsNewTransactionModalOpen(true);
+        },
+        []
+    );
+
+    const closeNewTransactionModal = useCallback(() => {
+        setNewTransactionType(null);
+        setIsNewTransactionModalOpen(false);
+    }, []);
+
     return (
         <DashboardContext.Provider
             value={{
@@ -35,6 +57,10 @@ export function DashBoardProvider({ children }: { children: React.ReactNode }) {
                 closeNewAccountModal,
                 isNewAccountModalOpen,
                 openNewAccountModal,
+                isNewTransactionModalOpen,
+                closeNewTransactionModal,
+                openNewTransactionModal,
+                newTransactionType,
             }}
         >
             {children}
